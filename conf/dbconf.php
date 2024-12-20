1af017e55c92b511766328bfc25f776fd4d2ed8c
GayanukaT <139636318+GayanukaT@users.noreply.github.com>
2024-12-20 09:49:52 +0000

    Handled some errors

--- conf/dbconf.php
+++ conf/dbconf.php
@@ -2,7 +2,7 @@
 define('SERVERNAME','127.0.0.1');
 define('USERNAME','root');
 define('PASSWORD','mariadb');
-define('DBNAME','Seeds');
+define('DBNAME','online_seed_marketplace');
 
 try {
     $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
